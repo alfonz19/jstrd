cc393973a8bc2fd270016c9b233ed48b61c8c1ff
Martin Mucha <martin.mucha@embedit.cz>
2022-03-07 20:38:21 +0100

    testing setting color using StreamDeckCore way
    dysfunctional on linux, the buffer size
    sent to device is bigger that what is
    allowed by purejavahidapi library. Both in
    original variant and vveird variant.

--- src/main/java/alf/Main.java
+++ src/main/java/alf/Main.java
@@ -6,8 +6,12 @@ import purejavahidapi.HidDeviceInfo;
 import purejavahidapi.InputReportListener;
 import purejavahidapi.PureJavaHidApi;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.io.IOException;
-import java.rmi.ServerError;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -114,46 +118,21 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             outer: while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                switch (line) {
-                    case "quit": break outer;
-                    default:
-                        System.out.println("unknown command");
+
+                if (line.equals("quit")) { break; }
+
+                try {
+                    Method method = Main.class.getMethod(line);
+                    method.invoke(this);
+                } catch (NoSuchMethodException e) {
+                    System.err.println("unable to invoke method: "+line);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
-
-//            try {
-//                while (!terminate) {
-//                    try {
-//                        Thread.sleep(500);
-//                    } catch (InterruptedException e) {
-//                        Thread.currentThread().interrupt();
-//                    }
-//                }
-//
-//                log.info("main thread done.");
-//            } catch (Exception e) {
-//                throw new RuntimeException("whoa, what was that", e);
-//            }
         } catch (IOException e) {
             throw new RuntimeException("Unable to open", e);
         }
-
-
-//---------------------
-//        try {
-//            hidDevice = PureJavaHidApi.openDevice(info);
-//            try {
-//
-//            } finally {
-//                try {
-//                    hidDevice.close();
-//                } catch (Exception e) {
-//                    log.error("error closing.");
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException("Unable to open", e);
-//        }
     }
 
     private void printInfoAboutDevice(HidDeviceInfo info) {
@@ -194,5 +173,143 @@ public class Main {
         log.error("Streamdeck not found");
         System.exit(1);
         return null;
+    }
+
+    //----------------------------------------------
+
+    private static final byte[] RESET_DATA = new byte[] { 0x0B, 0x63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    /**
+     * Header for Page 1 of the image command
+     */
+    private static final byte[] PAGE_1_HEADER = new byte[] { 0x01, 0x01, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x42, 0x4D, (byte) 0xF6, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00,
+            0x00, 0x28, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00,
+            0x00, 0x00, 0x00, (byte) 0xC0, 0x3C, 0x00, 0x00, (byte) 0xC4, 0x0E, 0x00, 0x00, (byte) 0xC4, 0x0E, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+    /**
+     * Header for Page 2 of the image command
+     */
+    private static final byte[] PAGE_2_HEADER = new byte[] { 0x01, 0x02, 0x00, 0x01, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+    /**
+     * Page size that can be sent to the ESD at once
+     */
+    public static final int PAGE_PACKET_SIZE = 8190;
+
+    /**
+     * Page 1 for the image command
+     */
+    private byte[] p1 = new byte[PAGE_PACKET_SIZE];
+
+    /**
+     * Page 2 for the image command
+     */
+    private byte[] p2 = new byte[PAGE_PACKET_SIZE];
+
+    /**
+     * Pixels(times 3 to get the amount of bytes) of an icon that can be sent with page 1 of the image command
+     */
+    public static final int NUM_FIRST_PAGE_PIXELS = 2583;
+
+    /**
+     * Pixels(times 3 to get the amount of bytes) of an icon that can be sent with page 2 of the image command
+     */
+    public static final int NUM_SECOND_PAGE_PIXELS = 2601;
+
+    /**
+     * Icon size of one key
+     */
+    public static final int ICON_SIZE = 72;
+
+    public void sdcReset() {
+        hidDevice.setFeatureReport(RESET_DATA[0], Arrays.copyOfRange(RESET_DATA, 1, RESET_DATA.length), RESET_DATA.length-1);
+    }
+
+    public synchronized void sdcColor() {
+        internalDrawImage(0, Color.RED);
+        internalDrawImage(1, Color.GREEN);
+        internalDrawImage(2, Color.BLACK);
+        internalDrawImage(3, Color.GRAY);
+        internalDrawImage(4, Color.BLACK);
+
+    }
+
+    private synchronized void internalDrawImage(int keyIndex, Color color) {
+        byte[] imgData = createColoredIcon(color);
+        byte[] page1 = generatePage1(keyIndex, imgData);
+        byte[] page2 = generatePage2(keyIndex, imgData);
+        this.hidDevice.setOutputReport((byte) 0x02, page1, page1.length);
+        this.hidDevice.setOutputReport((byte) 0x02, page2, page2.length);
+    }
+
+    /**
+     * Generates HID-Report Page 1/2 to update an image of one stream deck key
+     *
+     * @param keyId
+     *            Id of the key to be updated
+     * @param imgData
+     *            image data in the bgr-format
+     * @return HID-Report in byte format ready to be send to the stream deck
+     */
+    private byte[] generatePage1(int keyId, byte[] imgData) {
+        for (int i = 0; i < PAGE_1_HEADER.length; i++) {
+            p1[i] = PAGE_1_HEADER[i];
+        }
+        if (imgData != null) {
+            for (int i = 0; i < imgData.length && i < NUM_FIRST_PAGE_PIXELS * 3; i++) {
+                p1[PAGE_1_HEADER.length + i] = imgData[i];
+            }
+        }
+        p1[4] = (byte) (keyId + 1);
+        return p1;
+    }
+
+    /**
+     * Generates HID-Report Page 2/2 to update an image of one stream deck key
+     *
+     * @param keyId
+     *            Id of the key to be updated
+     * @param imgData
+     *            image data in the bgr-format
+     * @return HID-Report in byte format ready to be send to the stream deck
+     */
+    private byte[] generatePage2(int keyId, byte[] imgData) {
+        for (int i = 0; i < PAGE_2_HEADER.length; i++) {
+            p2[i] = PAGE_2_HEADER[i];
+        }
+        if (imgData != null) {
+            for (int i = 0; i < NUM_SECOND_PAGE_PIXELS * 3 && i < imgData.length; i++) {
+                p2[PAGE_2_HEADER.length + i] = imgData[(NUM_FIRST_PAGE_PIXELS * 3) + i];
+            }
+        }
+        p2[4] = (byte) (keyId + 1);
+        return p2;
+    }
+
+    private static byte[] createColoredIcon(Color color) {
+        BufferedImage img = new BufferedImage(ICON_SIZE, ICON_SIZE, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = img.createGraphics();
+        g.setColor(color);
+        g.fillRect(0, 0, ICON_SIZE, ICON_SIZE);
+        g.dispose();
+        return bufferedImageToByteArrayData(img);
+    }
+
+    private static byte[] bufferedImageToByteArrayData(BufferedImage img) {
+        int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+        byte[] imgData = new byte[ICON_SIZE * ICON_SIZE * 3];
+        int imgDataCount = 0;
+        // remove the alpha channel
+        for (int i = 0; i < ICON_SIZE * ICON_SIZE; i++) {
+            // RGB -> BGR
+            imgData[imgDataCount++] = (byte) ((pixels[i] >> 16) & 0xFF);
+            imgData[imgDataCount++] = (byte) (pixels[i] & 0xFF);
+            imgData[imgDataCount++] = (byte) ((pixels[i] >> 8) & 0xFF);
+        }
+
+        return imgData;
     }
 }
