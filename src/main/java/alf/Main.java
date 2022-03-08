80e076ed68dec8428a1fd1195a1583721dc1b98e
Martin Mucha <martin.mucha@embedit.cz>
2022-03-08 18:48:13 +0100

    removed defuncts StreamDeckCore methods

--- src/main/java/alf/Main.java
+++ src/main/java/alf/Main.java
@@ -140,7 +140,6 @@ public class Main {
                 }
             }
 
-//            riColor1();
         } catch (IOException e) {
             throw new RuntimeException("Unable to open", e);
         }
@@ -186,130 +185,6 @@ public class Main {
         return null;
     }
 
-    //----------------------------------------------
-
-    private static final byte[] RESET_DATA = new byte[] { 0x0B, 0x63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-    /**
-     * Header for Page 1 of the image command
-     */
-    private static final byte[] PAGE_1_HEADER = new byte[] { 0x01, 0x01, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x42, 0x4D, (byte) 0xF6, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00,
-            0x00, 0x28, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00,
-            0x00, 0x00, 0x00, (byte) 0xC0, 0x3C, 0x00, 0x00, (byte) 0xC4, 0x0E, 0x00, 0x00, (byte) 0xC4, 0x0E, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
-    /**
-     * Header for Page 2 of the image command
-     */
-    private static final byte[] PAGE_2_HEADER = new byte[] { 0x01, 0x02, 0x00, 0x01, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
-    /**
-     * Page size that can be sent to the ESD at once
-     */
-    public static final int PAGE_PACKET_SIZE = 8190;
-
-    /**
-     * Page 1 for the image command
-     */
-    private byte[] p1 = new byte[PAGE_PACKET_SIZE];
-
-    /**
-     * Page 2 for the image command
-     */
-    private byte[] p2 = new byte[PAGE_PACKET_SIZE];
-
-    /**
-     * Pixels(times 3 to get the amount of bytes) of an icon that can be sent with page 1 of the image command
-     */
-    public static final int NUM_FIRST_PAGE_PIXELS = 2583;
-
-    /**
-     * Pixels(times 3 to get the amount of bytes) of an icon that can be sent with page 2 of the image command
-     */
-    public static final int NUM_SECOND_PAGE_PIXELS = 2601;
-
-    /**
-     * Icon size of one key
-     */
-    public static final int ICON_SIZE = 72;
-
-    public void sdcReset() {
-        hidDevice.setFeatureReport(RESET_DATA[0], Arrays.copyOfRange(RESET_DATA, 1, RESET_DATA.length), RESET_DATA.length-1);
-    }
-
-    public synchronized void sdcColor() {
-        internalDrawImage(0, Color.RED);
-        internalDrawImage(1, Color.GREEN);
-        internalDrawImage(2, Color.BLACK);
-        internalDrawImage(3, Color.GRAY);
-        internalDrawImage(4, Color.BLACK);
-
-    }
-
-    private synchronized void internalDrawImage(int keyIndex, Color color) {
-        byte[] imgData = createColoredIcon(color, ICON_SIZE);
-        byte[] page1 = generatePage1(keyIndex, imgData);
-        byte[] page2 = generatePage2(keyIndex, imgData);
-        this.hidDevice.setOutputReport((byte) 0x02, page1, page1.length);
-        this.hidDevice.setOutputReport((byte) 0x02, page2, page2.length);
-    }
-
-    /**
-     * Generates HID-Report Page 1/2 to update an image of one stream deck key
-     *
-     * @param keyId
-     *            Id of the key to be updated
-     * @param imgData
-     *            image data in the bgr-format
-     * @return HID-Report in byte format ready to be send to the stream deck
-     */
-    private byte[] generatePage1(int keyId, byte[] imgData) {
-        for (int i = 0; i < PAGE_1_HEADER.length; i++) {
-            p1[i] = PAGE_1_HEADER[i];
-        }
-        if (imgData != null) {
-            for (int i = 0; i < imgData.length && i < NUM_FIRST_PAGE_PIXELS * 3; i++) {
-                p1[PAGE_1_HEADER.length + i] = imgData[i];
-            }
-        }
-        p1[4] = (byte) (keyId + 1);
-        return p1;
-    }
-
-    /**
-     * Generates HID-Report Page 2/2 to update an image of one stream deck key
-     *
-     * @param keyId
-     *            Id of the key to be updated
-     * @param imgData
-     *            image data in the bgr-format
-     * @return HID-Report in byte format ready to be send to the stream deck
-     */
-    private byte[] generatePage2(int keyId, byte[] imgData) {
-        for (int i = 0; i < PAGE_2_HEADER.length; i++) {
-            p2[i] = PAGE_2_HEADER[i];
-        }
-        if (imgData != null) {
-            for (int i = 0; i < NUM_SECOND_PAGE_PIXELS * 3 && i < imgData.length; i++) {
-                p2[PAGE_2_HEADER.length + i] = imgData[(NUM_FIRST_PAGE_PIXELS * 3) + i];
-            }
-        }
-        p2[4] = (byte) (keyId + 1);
-        return p2;
-    }
-
-    private static byte[] createColoredIcon_defunct(Color color, int iconSize) {
-        BufferedImage img = new BufferedImage(iconSize, iconSize, /*BufferedImage.TYPE_INT_ARGB*/BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = img.createGraphics();
-        g.setColor(color);
-        g.fillRect(0, 0, iconSize, iconSize);
-        byte[] bytes = bufferedImageToByteArrayData(img, iconSize);
-        g.dispose();
-        return bytes;
-    }
-
     private static byte[] createColoredIcon(Color color, int iconSize) {
         int width = iconSize;
         int height = iconSize;
@@ -327,25 +202,6 @@ public class Main {
             throw new RuntimeException("write failed");
         }
     }
-
-
-    private static byte[] bufferedImageToByteArrayData(BufferedImage img, int iconSize) {
-        int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
-        byte[] imgData = new byte[iconSize * iconSize * 3];
-        int imgDataCount = 0;
-        // remove the alpha channel
-        for (int i = 0; i < iconSize * iconSize; i++) {
-            // RGB -> BGR
-            imgData[imgDataCount++] = (byte) ((pixels[i] >> 16) & 0xFF);
-            imgData[imgDataCount++] = (byte) (pixels[i] & 0xFF);
-            imgData[imgDataCount++] = (byte) ((pixels[i] >> 8) & 0xFF);
-        }
-
-        return imgData;
-    }
-
-
-    //-----------------------------------------------------------------------------------------------------
 
     /**
      * sets some color to every button
